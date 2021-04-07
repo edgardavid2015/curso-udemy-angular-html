f5b95d41a8ac0b52befb858e802d856cedb6bbc8
Edgar Guaman <edguitar2164@gmail.com>
2021-04-07 03:52:42 -0500

    Busqueda y filtrado de productos

--- portafolio/src/app/services/productos.service.ts
+++ portafolio/src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) {
     this.cargarProductos();
@@ -16,13 +17,45 @@ export class ProductosService {
 
 
   private cargarProductos(){
-
-    this.http.get('https://angular-html-f0baa-default-rtdb.firebaseio.com/productos_idx.json')
+    return new Promise((resolve,reject)=>{
+      this.http.get('https://angular-html-f0baa-default-rtdb.firebaseio.com/productos_idx.json')
     .subscribe((resp: any)=>{
-      console.log(resp);
       this.productos = resp;
       this.cargando = false;
+      resolve(true);
     });
+    })
+  }
+
+  getProducto(id:string){
+    return this.http.get(`https://angular-html-f0baa-default-rtdb.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino:string){
+    if(this.productos.length === 0){
+      this.cargarProductos().then(()=>{
+        this.filtrarProductos(termino);
+      });
+    }else{
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos(termino:string){
+    console.log(this.productos);
+    this.productosFiltrado = [];
+    termino = termino.toLowerCase();
+    this.productos.forEach(prod =>{
+      const tituloLower = prod.titulo.toLowerCase();
+      if(prod.categoria.indexOf(termino)>=0 || tituloLower.indexOf(termino)>=0){
+        this.productosFiltrado.push(prod);
+      }
+    })
+    
 
   }
+  
+  
+
+
 }
